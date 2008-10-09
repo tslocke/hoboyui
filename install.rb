9865f1fc686f0b16a437b6eb6f6960caa0282704
Tom Locke <tom@tomlocke.com>
2008-10-09 13:51:07 +0100

    Fix to installing js file on install

--- install.rb
+++ install.rb
@@ -2,4 +2,4 @@ require 'fileutils'
 
 HOBO_YUI_HOME = File.dirname(__FILE__)
 
-FileUtils.cp_r("#{HOBO_YUI_HOME}/public", "#{RAILS_ROOT}/public")
+FileUtils.cp_r("#{HOBO_YUI_HOME}/public", RAILS_ROOT)
